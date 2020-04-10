d4ed13a1353e403ebfaffe237a80861549ea914e
drizzer14 <drizzer14@gmail.com>
2020-04-10 15:28:45 +0300

    feat: Add request config

---
+++ config/index.ts
@@ -0,0 +1,26 @@
+import { AxiosRequestConfig } from 'axios';
+import { Category, CategoryVersion } from '../types';
+
+const serviceProtocol = 'http://';
+const serviceIP = '185.25.116.133';
+const servisePort = 8989;
+const serviceAddress = `${serviceProtocol}${serviceIP}:${servisePort}`;
+
+const createRequestConfig = ({
+  method,
+  path,
+  params = [],
+}: {
+  method: AxiosRequestConfig['method'];
+  path: string;
+  params?: string[];
+}): AxiosRequestConfig => ({
+  method,
+  url: `${serviceAddress}/${path}/${params.join('/')}`,
+  responseType: 'json',
+});
+
+export const getCategoryVersionConfig = (
+  categoryId: Category['id'],
+  version: CategoryVersion['version']
+): AxiosRequestConfig => createRequestConfig({ method: 'get', path: 'categories', params: [categoryId, version] });
