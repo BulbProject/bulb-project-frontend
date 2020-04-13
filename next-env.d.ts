8e4af3ea9643f82138a60424af26bf050ea994e3
hanna <annisokay97@gmail.com>
2020-04-13 17:08:36 +0300

    Feat(SVG): Add config for svg

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+declare module '*.svg' {
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+}
--- next.config.js
+++ next.config.js
@@ -6,4 +6,21 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
   },
   exportTrailingSlash: true,
+  webpack: config => {
+    config.module.rules.push(
+      ...[
+        {
+          test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
+          loader: require.resolve('file-loader'),
+          options: {
+            name: 'assets/[name].[hash:8].[ext]',
+          },
+        },
+      ]
+    );
+
+    return config;
+  },
 });
+
+
