88fb7d4f25df5297230365ae5bad24dc7d6f8263
hanna <annisokay97@gmail.com>
2020-04-13 17:12:29 +0300

    Fix(Classifications): Add styles for classification id

--- components/Classification/index.tsx
+++ components/Classification/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Classification as OCDSClassification } from 'ts4ocds';
-import { Flex, Text } from 'ustudio-ui';
+import { Flex } from 'ustudio-ui';
 
 import Styled from './styles';
 
 const Classification: React.FC<OCDSClassification & { className?: string }> = ({ id, description, className = '' }) => {
   return (
     <Flex className={className}>
-      <Text variant="small">{id}</Text>
+      <Styled.Id variant="small">{id}</Styled.Id>
 
       {description && <Styled.Description variant="small">{description}</Styled.Description>}
     </Flex>
