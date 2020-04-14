16a24849b69057ce344439efcdee0facca40bdbf
Dmitriy Volkov <mr.rotberry@gmail.com>
2020-04-14 19:11:19 +0300

    feat(Config): Add request configs for github and renamed API Service configs

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,47 @@
 import { AxiosRequestConfig } from 'axios';
 import { Category, CategoryVersion } from 'types/data';
 
-const serviceProtocol = 'http';
-const serviceIP = '185.25.116.133';
-const servisePort = 8989;
-const serviceAddress = `${serviceProtocol}://${serviceIP}:${servisePort}`;
+const apiServiceProtocol = 'http';
+const apiServiceHost = '185.25.116.133';
+const apiServicePort = 8989;
+const apiServiceBaseUrl = `${apiServiceProtocol}://${apiServiceHost}${apiServicePort ? `:${apiServicePort}` : ''}`;
 
 const createRequestConfig = ({
+  baseUrl,
   method,
   path,
   params = [],
 }: {
+  baseUrl: string;
   method: AxiosRequestConfig['method'];
   path: string;
   params?: string[];
 }): AxiosRequestConfig => ({
   method,
-  url: `${serviceAddress}/${path}/${params.join('/')}`,
+  url: `${baseUrl}/${path}${params?.length ? params.join('/') : ''}`,
   responseType: 'json',
 });
 
 export const getCategoryVersionConfig = (
   categoryId: Category['id'],
   version: CategoryVersion['version']
-): AxiosRequestConfig => createRequestConfig({ method: 'get', path: 'categories', params: [categoryId, version] });
+): AxiosRequestConfig => {
+  return createRequestConfig({
+    baseUrl: apiServiceBaseUrl,
+    method: 'get',
+    path: 'categories',
+    params: [categoryId, version],
+  });
+};
+
+const githubBaseUrl = 'https://api.github.com/repos/BulbProject/bulb-project-frontend/contents';
+const githubBranch = 'info-pages';
+const githubInfoPath = 'info';
+
+export const getInfoFiles = () => {
+  return createRequestConfig({
+    baseUrl: githubBaseUrl,
+    method: 'get',
+    path: `2${githubInfoPath}?ref=${githubBranch}`,
+  });
+};
