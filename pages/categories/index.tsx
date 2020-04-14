5bb0f89a2e814711fb6d64580ebfb9048f508b01
hanna <annisokay97@gmail.com>
2020-04-14 14:29:59 +0300

    Refactor:  Remove logic of fetching categories list to front

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { NextPage } from 'next';
-import { Text, Flex, Cell } from 'ustudio-ui';
+import React, { useEffect, useState } from 'react';
+import { Text, Flex, Cell, Spinner } from 'ustudio-ui';
 
 import Styled from '../../styles/categories';
 import { requestData } from '../../utils';
@@ -10,10 +9,27 @@ import { RequestError } from '../../types';
 import Card from '../../components/categories/Cards/Card';
 import Error from './../../components/categories/Error';
 
-const Categories: NextPage<{ categoriesList?: CategoriesListEntity[]; error?: RequestError }> = ({
-  categoriesList = [],
-  error,
-}) => {
+type ReqError = RequestError | undefined;
+
+const Categories = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [categoriesList, setCategoriesList] = useState((null as unknown) as CategoriesListEntity[]);
+  const [error, setError] = useState(undefined as ReqError);
+
+  const getCategoriesList = async () => {
+    setLoading(true);
+    const { data: categories, error } = await requestData<CategoriesListEntity[]>(getCategoriesConfig());
+
+    if (categories) setCategoriesList(categories);
+
+    setError(error);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCategoriesList();
+  }, []);
+
   return (
     <Styled.Wrapper>
       <Styled.Container isContainer>
@@ -24,23 +40,24 @@ const Categories: NextPage<{ categoriesList?: CategoriesListEntity[]; error?: Re
             <Flex direction="column">
               <Styled.ListTitle variant="h1">Select category for future calculation</Styled.ListTitle>
 
-              {!categoriesList?.length && <Text variant="h3">There are no categories yet</Text>}
-
-              {categoriesList?.map(category => (
-                <Card key={category.id} id={category.id} version={category.version} date={category.date} />
-              ))}
+              {!isLoading ? (
+                <Flex alignment={{ vertical: 'center', horizontal: 'center' }}>
+                  <Spinner appearance={{ size: 52 }} />
+                </Flex>
+              ) : (
+                <>
+                  {!categoriesList?.length && <Text variant="h3">There are no categories yet</Text>}
+                  {categoriesList?.map(category => (
+                    <Card key={category.id} id={category.id} version={category.version} date={category.date} />
+                  ))}
+                </>
+              )}
             </Flex>
           )}
         </Cell>
       </Styled.Container>
     </Styled.Wrapper>
   );
-};
-
-Categories.getInitialProps = async () => {
-  const { data: categoriesList, error } = await requestData<CategoriesListEntity[]>(getCategoriesConfig());
-
-  return { categoriesList, error };
 };
 
 export default Categories;
