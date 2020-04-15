1a72b9bd312b208772c3a703bdc19e06fee80172
Dmitriy Volkov <mr.rotberry@gmail.com>
2020-04-15 18:23:35 +0300

    refactor: Use CSS variables in styles for renders markdown

--- src/components/markdown/styles.ts
+++ src/components/markdown/styles.ts
@@ -12,7 +12,7 @@ const Divider = styled.hr`
 `;
 
 const Heading = styled.h1`
-  margin: 2rem 0 0;
+  margin: var(--i-large) 0 0;
   scroll-margin: calc(54px + 2rem) 0 0;
 `;
 
@@ -23,8 +23,8 @@ const Paragraph = styled.div`
 
 const Quote = styled.blockquote`
   border-left: 2px solid var(--c-primary);
-  padding-left: 1rem;
-  margin: 1rem 0;
+  padding-left: var(--i-regular);
+  margin: var(--i-regular) 0;
   color: var(--c-dark);
 
   ${Paragraph} {
@@ -36,10 +36,10 @@ const List = styled.ul(
   ({ depth, ordered }: { depth: number; ordered: boolean }) => css`
     display: grid;
     grid-auto-rows: auto;
-    grid-gap: 0.5rem;
+    grid-gap: var(--i-medium);
 
-    margin: 0.25rem 0;
-    padding-left: ${depth ? '2rem' : 0};
+    margin: var(--i-small) 0;
+    padding-left: ${depth ? 'var(--i-large)' : 0};
 
     list-style-position: inside;
     list-style-type: ${ordered ? 'decimal' : 'circle'};
