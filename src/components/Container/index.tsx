a57dae5ca625f6b6dc3d228c78a11422672a05be
hanna <annisokay97@gmail.com>
2020-04-15 18:35:52 +0300

    refactor(Categories Page): update Container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -6,11 +6,9 @@ import { containerCellProps } from './config';
 import Styled from './styles';
 
 const Container: FC = ({ children }) => (
-  <Styled.Wrapper>
-    <Styled.Container isContainer>
-      <Cell xs={containerCellProps}>{children}</Cell>
-    </Styled.Container>
-  </Styled.Wrapper>
+  <Styled.Container isContainer>
+    <Cell xs={containerCellProps}>{children}</Cell>
+  </Styled.Container>
 );
 
 export default Container;
--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -2,9 +2,6 @@ import styled, { css } from 'styled-components';
 import { Grid } from 'ustudio-ui';
 import { Mixin } from 'ustudio-ui/theme';
 
-const Wrapper = styled.div`
-  margin: var(--i-large) 0;
-`;
 
 const Container = styled(Grid)`
   padding: 0 var(--i-large);
@@ -16,6 +13,5 @@ const Container = styled(Grid)`
 `;
 
 export default {
-  Wrapper,
   Container,
 };
