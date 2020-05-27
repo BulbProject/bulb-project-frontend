425f385d705fc7f8a2ffc9fd51b0948035401adc
hanna <annisokay97@gmail.com>
2020-05-27 10:32:11 +0300

    refactor: Add space

--- src/pages/categories-list/modules/CardLayout/CardLayout.styles.ts
+++ src/pages/categories-list/modules/CardLayout/CardLayout.styles.ts
@@ -24,6 +24,7 @@ const Cell = styled.div(({ image }: { image?: string }) => {
 const WideCardTitle = () => css`
   ${StyledCard.CardTitle} {
     ${Mixin.Font.h2()};
+
     font-weight: 400;
     line-height: 1.1;
   }
