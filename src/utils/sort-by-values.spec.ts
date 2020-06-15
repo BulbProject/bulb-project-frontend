29c6482389ecf8ab70b202aba6640c648d4d56e6
natalia-lypovyk <cardinnata@gmail.com>
2020-06-15 15:19:15 +0300

    refactor: jest dependecies moved to devDependencies, object literals readability

--- src/utils/sort-by-values.spec.ts
+++ src/utils/sort-by-values.spec.ts
@@ -2,6 +2,12 @@ import { sortByValue } from './index';
 
 describe('sortByValue', () => {
   it('Should sort an unsorted array of objects by the value of specified key', () => {
-    expect([{ 1: '0102' }, { 1: '0101' }].sort(sortByValue(1))).toStrictEqual([{ 1: '0101' }, { 1: '0102' }]);
+    expect([
+      { 1: '0102' },
+      { 1: '0101' }
+      ].sort(sortByValue(1))).toStrictEqual([
+        { 1: '0101' },
+      { 1: '0102' }
+      ]);
   });
 });
