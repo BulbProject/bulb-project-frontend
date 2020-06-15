4a3feba771f119a8c7f1a1d8e920eec61ea3f198
natalia-lypovyk <58652925+natalia-lypovyk@users.noreply.github.com>
2020-06-15 15:57:06 +0300

    tests(utils): add unit tests (#79)
    * feat: tests for format-number, get-random-width, modify-id
    
    * feat: tests for sort-by-values, get-locale-data-type
    
    * refactor: change descriptions, instances of error removed
    
    * refactor: all instances of errors removed
    
    * refactor: error instances, entity apos removed
    
    * refactor: move tests to utils
    
    * refactor: jest dependecies moved to devDependencies, object literals readability
    
    * chore: update package.json

---
+++ src/utils/get-locale-data-type.spec.ts
@@ -0,0 +1,35 @@
+import { getLocaleDataType } from './index';
+
+describe('getLocaleDataType', () => {
+  describe('When locale is not defined', () => {
+    it('Should return an empty string', () => {
+      expect(getLocaleDataType(undefined)).toBe('');
+    });
+
+    describe('When locale is UA', () => {
+      describe('When dataType is string', () => {
+        it('Should return текст', () => {
+          expect(getLocaleDataType('string', 'UA')).toBe('текст');
+        });
+      });
+
+      describe('When dataType is boolean', () => {
+        it('Should return так/ні', () => {
+          expect(getLocaleDataType('boolean', 'UA')).toBe('так/ні');
+        });
+      });
+
+      describe('When dataType is number', () => {
+        it('Should return число', () => {
+          expect(getLocaleDataType('number', 'UA')).toBe('число');
+        });
+      });
+
+      describe('When dataType is integer', () => {
+        it('Should return одиниць', () => {
+          expect(getLocaleDataType('integer', 'UA')).toBe('одиниць');
+        });
+      });
+    });
+  });
+});
---
+++ src/utils/get-random-width.spec.ts
@@ -0,0 +1,56 @@
+import { getRandomWidth } from './index';
+
+describe('getRandomWidth', () => {
+  jest.spyOn(Math, 'random').mockReturnValue(1);
+
+  describe('When min > max', () => {
+    it('Should throw an error', () => {
+      expect(() => getRandomWidth(5, 2)).toThrow(RangeError);
+    });
+  });
+
+  describe('When min and max are undefined', () => {
+    it('Should throw an error', () => {
+      // @ts-expect-error
+      expect(() => getRandomWidth(undefined, undefined)).toThrow(ReferenceError);
+    });
+  });
+
+  describe('When min === undefined', () => {
+    it('Should throw an error', () => {
+      // @ts-expect-error
+      expect(() => getRandomWidth(undefined, 5)).toThrow(ReferenceError);
+    });
+  });
+
+  describe('When max === undefined', () => {
+    it('Should throw an error', () => {
+      // @ts-expect-error
+      expect(() => getRandomWidth(3, undefined)).toThrow(ReferenceError);
+    });
+  });
+
+  describe('When min is equal to max', () => {
+    it('Should throw an error', () => {
+      expect(() => getRandomWidth(3, 3)).toThrow(RangeError);
+    });
+  });
+
+  describe('When min < 0', () => {
+    it('Should throw an error', () => {
+      expect(() => getRandomWidth(-1, 6)).toThrow(RangeError);
+    });
+  });
+
+  describe('When max < 0', () => {
+    it('Should throw an error', () => {
+      expect(() => getRandomWidth(2, -1)).toThrow(RangeError);
+    });
+  });
+
+  describe('With correct arguments', () => {
+    it('Should return random width', () => {
+      expect(getRandomWidth(2, 5)).toBe('5%');
+    });
+  });
+});
