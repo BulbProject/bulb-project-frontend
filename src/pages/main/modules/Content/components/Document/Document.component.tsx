75141d25ad7a3d44bd6def3c03f2c5915865a42e
natalia-lypovyk <cardinnata@gmail.com>
2020-06-16 12:54:59 +0300

    feat: useRequest update in Document component

--- src/pages/main/modules/Content/components/Document/Document.component.tsx
+++ src/pages/main/modules/Content/components/Document/Document.component.tsx
@@ -3,40 +3,39 @@ import ReactMarkdown from 'react-markdown';
 import Flex from 'ustudio-ui/components/Flex';
 import Spinner from 'ustudio-ui/components/Spinner';
 
-import { useRequest } from 'hooks';
+import { useRequest } from 'honks';
+import axios from 'axios';
 import { getMainContentFile } from 'config';
 import { FadeIn, renderers } from 'components';
 
 export const Document: React.FC<{ fileName: string }> = ({ fileName }) => {
-  const [isMounted, setMounted] = useState(false);
+  const { onPending, onSuccess, onFail, sendRequest } = useRequest<{ content: string }>(async () => {
+    const { data } = await axios(getMainContentFile(fileName.slice(0, -3)));
 
-  const { isLoading, error, data } = useRequest<{ content: string }>(getMainContentFile(fileName.slice(0, -3)), {
-    isRequesting: isMounted,
+    return data;
   });
 
   useEffect(() => {
-    setMounted(true);
-
-    return () => setMounted(false);
+    (async () => {
+      await sendRequest();
+    })();
   }, []);
-
-  if (isLoading && !error) {
-    return (
-      <FadeIn>
-        <Flex alignment={{ vertical: 'center', horizontal: 'center' }}>
-          <Spinner />
-        </Flex>
-      </FadeIn>
-    );
-  }
 
   return (
     <FadeIn>
-      <ReactMarkdown
-        escapeHtml={false}
-        source={data?.content || 'Не вдалося знайти цей документ'}
-        renderers={renderers}
-      />
+      {onPending(() => (
+        <Flex alignment={{ vertical: 'center', horizontal: 'center' }}>
+          <Spinner />
+        </Flex>
+      ))}
+
+      {onSuccess(({ content }) => (
+        <ReactMarkdown escapeHtml={false} source={content} renderers={renderers} />
+      ))}
+
+      {onFail(() => (
+        <ReactMarkdown escapeHtml={false} source="Не вдалося знайти цей документ" renderers={renderers} />
+      ))}
     </FadeIn>
   );
 };
