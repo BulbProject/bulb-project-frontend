c053067a05447cb2aee2cb9deb1e922f38340cff
natalia-lypovyk <cardinnata@gmail.com>
2020-06-16 13:59:21 +0300

    feat: useRequest update in Info component

--- src/pages/info/Info.component.tsx
+++ src/pages/info/Info.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useParams, useHistory } from 'react-router-dom';
 
@@ -10,7 +10,8 @@ import { Helmet } from 'react-helmet';
 
 import ReactMarkdown from 'react-markdown';
 
-import { useRequest } from 'hooks';
+import { useRequest } from 'honks';
+import axios, { AxiosError } from 'axios';
 import { getInfoFile } from 'config';
 
 import { Container } from 'shared';
@@ -24,67 +25,83 @@ import Styled from './Info.styles';
 const InfoComponent = () => {
   const { goBack } = useHistory();
   const { infoFileName } = useParams();
-  const { isLoading, data, error, triggerRequest } = useRequest<{ content: string }>(
-    getInfoFile(infoFileName as string),
-    {
-      dependencies: [infoFileName],
+  const { onPending, onSuccess, onFail, isFail, result, sendRequest } = useRequest<{ content: string }, AxiosError>(
+    async () => {
+      const { data } = await axios(getInfoFile(infoFileName as string));
+
+      return data;
     }
   );
+
+  useEffect(() => {
+    (async () => sendRequest())();
+  }, []);
 
   return (
     <Layout>
       <Container>
-        {isLoading && !error && (
-          <FadeIn>
-            <Styled.CenteredContainer>
-              <Spinner appearance={{ size: 64 }} delay={300} />
-            </Styled.CenteredContainer>
-          </FadeIn>
-        )}
-
-        {!isLoading && !error && (
-          <>
-            <Helmet>
-              <title>{infoFileName}</title>
-            </Helmet>
-
+        {onPending(() => {
+          return (
             <FadeIn>
-              <ReactMarkdown
-                escapeHtml={false}
-                source={data?.content || 'Не вдалося знайти цей документ'}
-                renderers={renderers}
-              />
+              <Styled.CenteredContainer>
+                <Spinner appearance={{ size: 64 }} delay={300} />
+              </Styled.CenteredContainer>
             </FadeIn>
-          </>
-        )}
+          );
+        })}
 
-        {!isLoading && error && (
-          <Flex direction="column" alignment={{ horizontal: 'center' }}>
-            <FadeIn>
-              <Flex margin={{ top: 'large', bottom: 'regular' }}>
-                <Text variant="h3" align="center" color="var(--c-negative)">
-                  Хм, щось пішло не так...
-                </Text>
-              </Flex>
+        {onSuccess(({ content }) => {
+          return (
+            <>
+              <Helmet>
+                <title>{infoFileName}</title>
+              </Helmet>
 
-              <Flex alignment={{ horizontal: 'center' }}>
-                <Styled.ActionButton onClick={() => goBack()}>
-                  <ArrowIcon />
-                  Назад
-                </Styled.ActionButton>
+              <FadeIn>
+                <ReactMarkdown
+                  escapeHtml={false}
+                  source={content || 'Не вдалося знайти цей документ'}
+                  renderers={renderers}
+                />
+              </FadeIn>
+            </>
+          );
+        })}
 
-                {error?.statusCode !== 404 && (
-                  <Flex isInline margin={{ left: 'regular' }}>
-                    <Styled.ActionButton onClick={() => triggerRequest()}>
-                      <ReloadIcon />
-                      Оновити сторінку
-                    </Styled.ActionButton>
-                  </Flex>
-                )}
-              </Flex>
-            </FadeIn>
-          </Flex>
-        )}
+        {onFail(() => {
+          return (
+            <Flex direction="column" alignment={{ horizontal: 'center' }}>
+              <FadeIn>
+                <Flex margin={{ top: 'large', bottom: 'regular' }}>
+                  <Text variant="h3" align="center" color="var(--c-negative)">
+                    Хм, щось пішло не так...
+                  </Text>
+                </Flex>
+
+                <Flex alignment={{ horizontal: 'center' }}>
+                  <Styled.ActionButton onClick={() => goBack()}>
+                    <ArrowIcon />
+                    Назад
+                  </Styled.ActionButton>
+
+                  {/* eslint-disable-next-line consistent-return */}
+                  {onFail(({ code }) => {
+                    if (code === '404') {
+                      return (
+                        <Flex isInline margin={{ left: 'regular' }}>
+                          <Styled.ActionButton onClick={sendRequest}>
+                            <ReloadIcon />
+                            Оновити сторінку
+                          </Styled.ActionButton>
+                        </Flex>
+                      );
+                    }
+                  })}
+                </Flex>
+              </FadeIn>
+            </Flex>
+          );
+        })}
       </Container>
     </Layout>
   );
