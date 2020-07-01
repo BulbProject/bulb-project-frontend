6505af9cf1bfc491d32a96d7257065f65eb0cabc
Volkov Dmitriy <mr.rotberry@gmail.com>
2020-07-01 16:35:00 +0300

    fix: Fix styles and setState for Fade component

--- src/shared/components/fade/fade.component.tsx
+++ src/shared/components/fade/fade.component.tsx
@@ -1,13 +1,19 @@
 import React, { FC, useEffect, useState } from 'react';
 
+import useMount from 'honks/use-mount';
+
 import Styled from './fade.styles';
 
 export const Fade: FC<{ duration?: number }> = ({ children, duration = 300 }) => {
   const [contentIsShow, setContentShow] = useState(false);
 
+  const hasMounted = useMount();
+
   useEffect(() => {
     setTimeout(() => {
-      setContentShow(true);
+      if (hasMounted()) {
+        setContentShow(true);
+      }
     }, duration / 2);
   }, []);
 
--- src/shared/components/fade/fade.styles.ts
+++ src/shared/components/fade/fade.styles.ts
@@ -2,6 +2,9 @@ import styled, { css } from 'styled-components';
 
 const Fade = styled.div<{ contentIsShow: boolean; duration: number }>(
   ({ contentIsShow, duration }) => css`
+    height: inherit;
+    width: inherit;
+
     opacity: ${contentIsShow ? 1 : 0};
 
     transition: opacity ${duration * 0.001}s;
