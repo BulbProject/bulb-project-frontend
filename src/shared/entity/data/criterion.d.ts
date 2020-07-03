fe193ca68f5b055cd146d10b97e46ad2979318f5
semenov-ol <semenov.os16@gmail.ocm>
2020-07-03 15:26:14 +0300

    fix: Fix criterion type

--- src/shared/entity/data/criterion.d.ts
+++ src/shared/entity/data/criterion.d.ts
@@ -1,6 +1,7 @@
-import { Criterion as OCDSCriterion } from 'ts4ocds/extensions/requirements';
+import { Criterion as OCDSCriterion, RequirementGroup } from 'ts4ocds/extensions/requirements';
+import { RequirementWithOptionDetails } from 'ts4ocds/extensions/options';
 
-export interface Criterion extends OCDSCriterion {
+export interface Criterion extends OCDSCriterion<RequirementGroup<RequirementWithOptionDetails>> {
   id: string;
   title: string;
 }
