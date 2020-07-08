d9799843fac18e0863f3bce72ac0ae439e72be21
drizzer14 <drizzer14@gmail.com>
2020-07-08 11:10:09 +0300

    fix(FormValidator): confirm failed validation for current step only
    Fixes ES-3721
    
    Signed-off-by: drizzer14 <drizzer14@gmail.com>

--- src/shared/context/form-validator/form-validator.context.tsx
+++ src/shared/context/form-validator/form-validator.context.tsx
@@ -1,16 +1,19 @@
 import React, { FC, createContext, useContext, useReducer } from 'react';
 
+import { useStepperState } from 'modules/category/stepper-state';
+
 import { FormValidatorValue, formValidatorReducer, FormValidatorDispatcher } from './entity';
 
 const FormValidatorContext = createContext<FormValidatorValue | undefined>(undefined);
 
 export const FormValidator: FC = ({ children }) => {
   const [state, dispatch] = useReducer(formValidatorReducer, {});
+  const { currentStep } = useStepperState();
 
   return (
     <FormValidatorContext.Provider
       value={{
-        hasValidationFailed: Boolean(Object.keys(state).length),
+        hasValidationFailed: Boolean(Object.keys(state).length) && Object.keys(state).includes(currentStep.id),
         state,
         dispatch: new FormValidatorDispatcher(dispatch),
       }}
