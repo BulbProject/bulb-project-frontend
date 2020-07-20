2b9ff45baed4983077627d9018bc36daa5b56b72
natalia-lypovyk <cardinnata@gmail.com>
2020-07-20 14:06:09 +0300

    feat: added variable for JSON.stringify result

--- src/shared/utils/is-form-filled-in.ts
+++ src/shared/utils/is-form-filled-in.ts
@@ -1,9 +1,11 @@
 export const isFormFilledIn = (data: unknown): boolean => {
-  return !JSON.stringify(data, (key, value) => {
+  const formValue = JSON.stringify(data, (key, value) => {
     if (value === undefined || Number.isNaN(Number(value))) {
       return 'undefined';
     }
 
     return value;
-  }).includes('undefined');
+  });
+
+  return !formValue.includes('undefined');
 };
