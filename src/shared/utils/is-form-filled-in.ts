030085265d8fa8eaf81add904d67ef4bb38a54de
natalia-lypovyk <cardinnata@gmail.com>
2020-07-31 14:39:16 +0300

    fix: typeof value

--- src/shared/utils/is-form-filled-in.ts
+++ src/shared/utils/is-form-filled-in.ts
@@ -1,6 +1,6 @@
 export const isFormFilledIn = (data: unknown): boolean => {
   const formData = JSON.stringify(data, (key, value) => {
-    if (value === undefined || Number.isNaN(value)) {
+    if (typeof value === 'undefined' || Number.isNaN(value)) {
       return 'undefined';
     }
 
