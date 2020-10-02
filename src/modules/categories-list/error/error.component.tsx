7a2cf45a8d033b32aa6966ff7c100d162b29ffe1
semenov-ol <semenov.os16@gmail.com>
2020-10-02 13:57:08 +0300

    refactor: Remove variable

--- src/modules/categories-list/error/error.component.tsx
+++ src/modules/categories-list/error/error.component.tsx
@@ -4,15 +4,18 @@ import Button from 'ustudio-ui/components/Button';
 import Flex from 'ustudio-ui/components/Flex';
 import Text from 'ustudio-ui/components/Text';
 
+import { useTranslation } from 'react-i18next';
 import Styled from './error.styles';
 
 export const Error: FC<{
   reloadCategories(): void;
 }> = ({ reloadCategories }) => {
+  const { t } = useTranslation();
+
   return (
     <Styled.ErrorContainer>
       <Flex direction="column" alignment={{ horizontal: 'center' }}>
-        <Text color="var(--c-contrast-strong)">На жаль, ми не замогли завантажити список категорій.</Text>
+        <Text color="var(--c-contrast-strong)">{t('error')}</Text>
 
         <Flex alignment={{ horizontal: 'center' }} margin={{ top: 'large' }}>
           <Button intent="positive" onClick={reloadCategories}>
--- src/shared/components/language-select/language-select.component.tsx
+++ src/shared/components/language-select/language-select.component.tsx
@@ -4,8 +4,9 @@ import { useTranslation } from 'react-i18next';
 import Styled from './language-select.styles';
 
 export const LanguageSelect: FC = () => {
-  const [language, setLanguage] = useState('en');
   const { i18n } = useTranslation();
+
+  const [language, setLanguage] = useState(i18n.language);
 
   const changeLanguage = ({ target: { value } }: ChangeEvent<HTMLSelectElement>): void => {
     i18n.changeLanguage(value).catch(console.error);
@@ -14,18 +15,16 @@ export const LanguageSelect: FC = () => {
 
   const languages = ['en', 'ua'];
 
-  const options = languages.map((lang) => {
-    return (
-      <option key={lang} value={lang}>
-        {lang}
-      </option>
-    );
-  });
-
   return (
     <Styled.SelectContainer>
       <select value={language} onChange={changeLanguage}>
-        {options}
+        {languages.map((lang) => {
+          return (
+            <option key={lang} value={lang}>
+              {lang}
+            </option>
+          );
+        })}
       </select>
     </Styled.SelectContainer>
   );
