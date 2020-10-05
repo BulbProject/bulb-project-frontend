879aa03aaf23736d3a67d3ec9d974e8bbf769458
Volkov Dmitriy <mr.rotberry@gmail.com>
2020-10-05 18:10:23 +0300

    fix: Fix error message for calculation

--- src/shared/context/calculation/calculation.context.tsx
+++ src/shared/context/calculation/calculation.context.tsx
@@ -110,7 +110,7 @@ const Calculation: FC = ({ children }) => {
               `,
             }}
           >
-            <Text>{error.response?.data.message || error.message}</Text>
+            <Text>{error.response?.data.messages || error.message}</Text>
           </Modal>
         );
       })}
