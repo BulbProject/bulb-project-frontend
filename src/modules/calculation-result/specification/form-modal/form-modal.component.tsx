6ee42241c8ab4fce709e36d9ad8af54a94175ed2
semenov-ol <semenov.os16@gmail.com>
2020-10-06 11:56:27 +0300

    refactor: Change key documentation

--- src/modules/calculation-result/specification/form-modal/form-modal.component.tsx
+++ src/modules/calculation-result/specification/form-modal/form-modal.component.tsx
@@ -1,7 +1,7 @@
 import React, { FC, ReactElement } from 'react';
 import { css } from 'styled-components';
 import { Criterion, Requirement } from 'ts4ocds/extensions/requirements';
-import { useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import Button from 'ustudio-ui/components/Button';
 import Flex from 'ustudio-ui/components/Flex';
@@ -40,13 +40,13 @@ export const FormModal: FC<{
   setRequirement,
   setCopying,
 }) => {
-  const { t } = useTranslation('form-modal');
+  const { t } = useTranslation('common');
 
   return (
     <Modal
       isOpen={isOpen}
       onChange={setOpen}
-      title={<Text variant="h5">{criterion?.title ?? t('documents')}</Text>}
+      title={<Text variant="h5">{criterion?.title ?? t('documentation')}</Text>}
       styled={{
         Modal: css`
             width: 100%;
