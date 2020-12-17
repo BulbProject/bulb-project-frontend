3adcbebbb18bd2730ad786bbd952383d6674f599
natalia-lypovyk <cardinnata@gmail.com>
2020-12-18 01:40:02 +0200

    fix: Correct OptionGroup type

--- src/shared/entity/data/option-group.d.ts
+++ src/shared/entity/data/option-group.d.ts
@@ -1,5 +1,6 @@
 import type { OptionGroup } from 'ts4ocds/extensions/options';
+import type { OptionType } from './option';
 
 export interface OptionGroupType extends OptionGroup {
-  relatedRequirementId?: string;
+  options: OptionType[];
 }
