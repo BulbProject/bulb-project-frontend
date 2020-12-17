b082c5465a4b9d62bcc9c423016744f0f462a2ae
natalia-lypovyk <cardinnata@gmail.com>
2020-12-17 11:50:40 +0200

    refactor: Wrap options

--- src/shared/components/requirement/input/utils.ts
+++ src/shared/components/requirement/input/utils.ts
@@ -1,13 +1,17 @@
-import { Option } from 'ts4ocds/extensions/options';
 import { Item } from 'ustudio-ui/components/Select/select.types';
 
 import { sortByValue } from 'shared/utils';
+import { OptionType } from 'shared/entity/data';
 
-export const mapOptionsToItems = (options: Option[]): Record<string, Item> => {
+export const mapOptionsToItems = (options: OptionType[]): Record<string, Item> => {
   return options.sort(sortByValue('id')).reduce(
     (map, option) =>
       Object.assign(map, {
-        [option.value as string]: { value: option.value, label: option.title },
+        [option.value as string]: {
+          value: option.value,
+          label: option.title,
+          relatedRequirementId: option.relatedRequirementID,
+        },
       }),
     {}
   );
