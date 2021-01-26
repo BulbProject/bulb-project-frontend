e3a1896b858c607bad9024d2abd761f6eaf84af6
Natalia Lypovyk <cardinnata@gmail.com>
2021-01-26 12:37:21 +0200

    fix: Correct branch in github config

--- src/shared/config/github-api.config.ts
+++ src/shared/config/github-api.config.ts
@@ -1,7 +1,6 @@
 const gitHubApiConfig = {
   baseUrl: 'https://udoc.eprocurement.systems/entries/BulbProject/bulb-project-frontend',
-  // Process.env.APP_ENV === 'STAGE' ? 'master' : 'develop',
-  branch: 'tutorial',
+  branch: process.env.APP_ENV === 'STAGE' ? 'master' : 'develop',
 };
 
 export default gitHubApiConfig;
